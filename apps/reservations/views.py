e87cf0437f10cbf6abacaa9af0a01655351ef9e4
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-07-04 15:43:15 -0500

    fix: permission_class TableViewSet only AdminUser

--- apps/reservations/views.py
+++ apps/reservations/views.py
@@ -9,7 +9,7 @@ from datetime import timedelta
 class TableViewSet(viewsets.ModelViewSet):
     query = Table.objects.all()
     serializer_class = TableSerializer
-    permission_classes = [permissions.IsAdmin]
+    permission_classes = [permissions.IsAdminUser]
 
 class ReservationViewSet(viewsets.ModelViewSet):
     serializer_class = ReservationSerializer
