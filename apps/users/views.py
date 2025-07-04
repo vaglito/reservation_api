30b57cd9ace42d9089be59bd049b1d4617e72bc4
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-07-04 15:15:26 -0500

    Implement RegisterView for user registration with AllowAny permission

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
+from rest_framework import permissions
+from rest_framework.permissions import AllowAny
+from .serializers import RegisterSerializer
 
 # Create your views here.
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_class = [AllowAny]
