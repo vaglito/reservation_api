92ce0387364c0ee6e16050498e7824b922d2d358
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-07-04 14:47:12 -0500

    Add serializers for Table and Reservation models

---
+++ apps/reservations/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Table, Reservation
+
+class TableSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Table
+        fields = '__all__'
+
+class ReservationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reservation
+        fields = '__all__'
+        read_only_fields = ['user']
+        
