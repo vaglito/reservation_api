6601aa8ae9019660eee73e060d7d89671b6c1e52
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-07-04 15:15:22 -0500

    Add RegisterSerializer for user registration with password validation

---
+++ apps/users/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+
+class RegisterSerializer(serializers.ModelSerializer):
+    first_name = serializers.CharField(required=True)
+    last_name = serializers.CharField(required=True)
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'password',
+            'password2',
+            'email',
+            'frist_name',
+            'last_name',
+        )
+    
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError(
+                {
+                    "password": "Las contraseñas no coinciden."
+                }
+            )
+        return attrs
+    
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        user = User.objects.create_user(**validated_data)
+        return user
