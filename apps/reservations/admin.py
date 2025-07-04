ff91a4cfdc4d5737d72eb4c8900a88951a33a183
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-07-04 15:42:43 -0500

    add manager admin for models Table and Reservation

--- apps/reservations/admin.py
+++ apps/reservations/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
-
+from .models import Reservation, Table
 # Register your models here.
+
+
+@admin.register(Table)
+class TableAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'number',
+        'capacity'
+    ]
+    list_display_links = [
+        'id',
+        'number'
+    ]
+    search_fields = [
+        'number',
+    ]
+
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'user',
+        'table',
+        'reserved_at',
+        'duration_hours',
+    ]
+    search_fields = [
+        'user__email',
+        'user__username',
+        'table__number'
+    ]
+    list_filter = ['reserved_at', 'table']
+    list_display_links = [
+        'id',
+        'table',
+    ]
