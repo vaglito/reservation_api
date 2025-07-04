d51b88f4f2cf10c8c2362346af7a9bf25ca3b119
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-07-04 15:03:34 -0500

    Fix typo in URL routing for TableViewSet registration

--- apps/reservations/urls.py
+++ apps/reservations/urls.py
@@ -2,7 +2,7 @@ from rest_framework.routers import DefaultRouter
 from .views import TableViewSet, ReservationViewSet
 
 router = DefaultRouter()
-router.register(r'tables', TableViewSet, banename='tables')
+router.register(r'tables', TableViewSet, basename='tables')
 router.register(r'reservations', ReservationViewSet, basename='reservations')
 
 urlpatterns = router.urls
